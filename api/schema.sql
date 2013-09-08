3fb7d9cccb890a89be3545c0a5568b98ae46df6d
Tomoatsu Shimada <shimada@walbrix.com>
2013-09-09 03:49:36 +0900

    it runs at least

--- api/schema.sql
+++ api/schema.sql
@@ -1,14 +1,15 @@
-create table file (
+create table files (
 	id char(40) primary key,
 	path text not null,
 	atime datetime not null,
 	ctime datetime not null,
 	mtime datetime not null,
+	size bigint not null,
 	updated_at datetime,
 	contents text,
 	fulltext key(path),
 	fulltext key(contents)
-) engine=MyISAM;
+) engine=mroonga;
 
 create table indexing_queue (
 	file_id char(40) primary key,
--- api/src/com/walbrix/oscar/api/RequestHandler.scala
+++ api/src/com/walbrix/oscar/api/RequestHandler.scala
@@ -6,43 +6,73 @@ import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RequestParam
+import java.sql.Timestamp
+import com.walbrix.spring.Row
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.jdbc.core.support.SqlLobValue
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestBody
+import java.io.InputStream
+import org.apache.commons.io.IOUtils
 
-case class File()
+case class File(id:String,path:String,atime:Timestamp,ctime:Timestamp,mtime:Timestamp,size:Long,updatedAt:Timestamp)
 
 @Controller
 @RequestMapping(Array("file"))
 @Transactional
 class RequestHandler extends RequestHandlerBase {
+  
+	implicit def long2timestamp(v:Long) = new Timestamp(v)
+	
+	implicit def row2file(row:Row):File =
+		File(row("id"), row("path"), row("atime"), row("ctime"), row("mtime"), row("size"),row("updated_at"))
 
 	@RequestMapping(value=Array(""), method = Array(RequestMethod.GET))
 	@ResponseBody
-	def get(@RequestParam(value="file_id_prefix",required=false) fileIdPrefix:String):Seq[String] = {
-	  Seq()
+	def get(@RequestParam(value="file_id_prefix",required=false) fileIdPrefix:String):Seq[(String,String)] = {
+		(fileIdPrefix match {
+		  case null => queryForSeq("select id,path from files")
+		  case _ => queryForSeq("select id,path from files where id like ?", fileIdPrefix + "%") 
+		}).map { row =>
+			(row("id"),row("path")):(String,String)
+		}
 	}
   
+	// curl http://localhost:8080/oscar/file/ -d "path=hoge.doc" -d "atime=1000" -d "ctime=1000" -d "mtime=1000" -d "size=1024"
 	@RequestMapping(value=Array(""), method = Array(RequestMethod.POST))
 	@ResponseBody
-	def post(path:String,atime:Long,ctime:Long,mtime:Long):(Boolean,Option[String]) = {
-	  (false, None)
+	def post(path:String,atime:Long,ctime:Long,mtime:Long,size:Long):TypedResult[String] = {
+		update("insert into files(id,path,atime,ctime,mtime,size,updated_at) values(sha1(?),?,?,?,?,?,now()) " + 
+		    "on duplicate key update atime=?,ctime=?,mtime=?,size=?,updated_at=now()",
+		    path, path, atime:Timestamp, ctime:Timestamp, mtime:Timestamp, size,
+		    atime:Timestamp, ctime:Timestamp, mtime:Timestamp, size) match {
+		  case 0 => false
+		  case _ => TypedResult.success(queryForString("select sha1(?)", path))
+		}
 	}
 	
 	@RequestMapping(value=Array("{file_id}"), method = Array(RequestMethod.DELETE))
 	@ResponseBody
-	def delete(fileId:String):(Boolean,Option[Any]) = {
-	  (false, None)
+	def delete(@PathVariable("file_id") fileId:String):(Boolean,Option[Any]) = {
+		update("delete from files where id=?", fileId) > 0
 	}
 	
 	@RequestMapping(value=Array("{file_id}"), method = Array(RequestMethod.POST), consumes=Array("text/plain"))
 	@ResponseBody
-	def contents(fileId:String, reader:java.io.Reader):(Boolean, Option[Any]) = {
+	def contents(@PathVariable("file_id") fileId:String, contents:InputStream):Result = {
+		
+		update("update files set contents=? where id=?", IOUtils.toString(contents, "UTF-8"), fileId) > 0
 	  // http://static.springsource.org/spring/docs/3.0.x/javadoc-api/org/springframework/web/bind/annotation/RequestHeader.html
 	  // http://static.springsource.org/spring/docs/current/javadoc-api/org/springframework/jdbc/core/support/SqlLobValue.html#SqlLobValue(java.io.Reader,%20int)
-	  (false, None)
 	}
-	
+
+	type FileWithSnippets = (File, String, String)
 	@RequestMapping(value=Array("search"), method = Array(RequestMethod.GET))
 	@ResponseBody
-	def search(q:String):Seq[File] = {
-	  Seq()
+	def search(q:String):(Int, Seq[FileWithSnippets]) = {
+		val wildq = "%" + q + "%"
+		(0, queryForSeq("select * from files where path like ? or contents like ?", wildq, wildq).map { row =>
+		  	(row:File, "title snippet", "body snippet")
+		})
 	}
 }
