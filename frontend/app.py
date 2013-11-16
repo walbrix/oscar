dd374b5f58fad1bbb99c9e651b92dda2e59f40b8
Tomoatsu Shimada <shimada@walbrix.com>
2013-11-17 05:28:23 +0900

    unirest's timeout changed to 30

--- frontend/app.py
+++ frontend/app.py
@@ -21,6 +21,8 @@ import smbconf
 
 private_address_regex = re.compile(r"(^127\.0\.0\.1)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)")
 
+unirest.timeout(30)
+
 def _recent(share_id, path_prefix = "/"):
     results = oscar.get("/file/%s/recent" % share_id, {"path_prefix":path_prefix})
     for result in results:
